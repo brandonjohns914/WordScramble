4e962061e02f1bee713af1b94e3cc580c338fb5c
Brandon Johns <56181912+brandonjohns914@users.noreply.github.com>
2023-06-12 19:46:12 -0700

    Add files via upload

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -15,15 +15,25 @@ struct ContentView: View {
     @State private var errorTitle = ""
     @State private var errorMessage = ""
     @State private var showingError = false
+    
+    @State private var score = 0
     var body: some View {
+        
         NavigationView
         {
+            
             List
             {
+                Section("Your score is: ")
+                {
+                    Text("\(score)")
+                        .foregroundColor(.blue)
+                }
                 Section
                 {
                     TextField("Enter your word", text: $newWord)
                         .autocapitalization(.none)
+                        .foregroundColor(.teal)
                 }
                 Section
                 {
@@ -32,10 +42,13 @@ struct ContentView: View {
                         HStack
                         {
                             Image(systemName: "\(word.count).circle")
+                                .foregroundColor(.teal)
                             Text(word)
+                                .foregroundColor(.teal)
                         }
                     }
                 }
+                
             }
             .navigationTitle(rootWord)
             .onSubmit(addNewWord)
@@ -47,10 +60,13 @@ struct ContentView: View {
             {
                 Text(errorMessage)
             }
+            .toolbar{
+                Button("New Game" , action: newGame)
+                    .foregroundColor(.red)
+            }
         }
-        
-        
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -59,96 +75,127 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-    extension ContentView
+extension ContentView
+{
+    func addNewWord()
     {
-        func addNewWord()
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        guard answer.count > 0 else {return}
+        
+        
+        guard isOriginal(word: answer) else
         {
-            let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
-            
-            guard answer.count > 0 else {return}
-            
-            
-            guard isOriginal(word: answer) else
-            {
-                wordError(title: "Word used already", message: "Be more original!")
-                return
-            }
-            
-            guard isPossible(word: answer) else {
-                wordError(title: "Word not possible", message: "You cannot spell that word from '\(rootWord)'!")
-                return
-            }
-            
-            guard isReal(word: answer ) else
-            {
-                wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
-                    return
-            }
-            
-            
-            withAnimation
-            {
-                usedWords.insert(answer, at: 0)
-                newWord = ""
-            }
+            wordError(title: "Word used already", message: "Be more original!")
+            return
         }
         
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You cannot spell that word from '\(rootWord)'!")
+            return
+            
+        }
         
-        func startGame()
+        guard isReal(word: answer ) else
         {
-            if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt")
-            {          // all words from start.txt file are in  startWords
-                if let startWords = try? String(contentsOf: startWordsURL)
-                {
-                    let allWords = startWords.components(separatedBy: "\n")
-                    rootWord = allWords.randomElement() ?? "silkworm"
-                    
-                    return
-                }
-            }
             
-         fatalError("Could not load start.txt from bundle")
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
         }
-        
-        
-        func isOriginal(word: String) -> Bool
+        if answer.count < 3
         {
-            !usedWords.contains(word)
-        }
-        
-        func isPossible(word: String) -> Bool
-        {
-            var tempWord = rootWord
+            wordError(title: "Word not possible", message: "Words must be greater than 3 letters")
+            return
             
-            for letter in word
-            {
-                if let possibleWord = tempWord.firstIndex(of: letter)
-                {
-                    tempWord.remove(at: possibleWord)
-                }
-                else
-                {
-                    return false
-                }
-            }
-            return true
         }
         
-        
-        func isReal(word: String) -> Bool {
-            let checker = UITextChecker()
-            let range = NSRange(location: 0, length: word.utf16.count)
-            let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
-
-            return misspelledRange.location == NSNotFound
-        }
-        
-        func wordError(title: String, message: String)
+        switch answer.count
         {
-            errorTitle = title
-            errorMessage = message
-            showingError = true
+        case 4:
+            score += 4
+        case 5:
+            score += 5
+        case 6:
+            score += 6
+        case 7:
+            score += 7
+        default:
+            score += 3
         }
         
+        
+        withAnimation
+        {
+            usedWords.insert(answer, at: 0)
+            newWord = ""
+        }
     }
     
+    
+    
+    func startGame()
+    {
+        
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt")
+        {          // all words from start.txt file are in  startWords
+            if let startWords = try? String(contentsOf: startWordsURL)
+            {
+                let allWords = startWords.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "silkworm"
+                return
+            }
+        }
+        
+        
+        fatalError("Could not load start.txt from bundle")
+    }
+    
+    
+    func isOriginal(word: String) -> Bool
+    {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool
+    {
+        var tempWord = rootWord
+        
+        for letter in word
+        {
+            if let possibleWord = tempWord.firstIndex(of: letter)
+            {
+                tempWord.remove(at: possibleWord)
+            }
+            else
+            {
+                return false
+            }
+        }
+        return true
+    }
+    
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String)
+    {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
+    }
+    
+    func newGame()
+    {
+        score = 0
+        usedWords.removeAll()
+        startGame()
+    }
+    
+}
+
